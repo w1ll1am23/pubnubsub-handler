2d692fb23c758942611fda5846b374c0d91f0600
William Scanlon <wjs.scanlon@gmail.com>
2016-11-22 19:07:07 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,21 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name='pubnubsub-handler',
     version='0.0.3',
-    description='Handles the PubNub subscriptions between PubNub and Home-Assistant',
+    description='Handles the PubNub subscriptions between PubNub and Home-Assistant for Wink',
     author='William Scanlon',
     py_modules=['pubnubsubhandler'],
     license='MIT',
-    classifiers=(
-        'Programming Language :: Python',
-        'License :: MIT License',
-        'Operating System :: OS Independent',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ),
     install_requires=[
         'pycryptodomex>=3.3',
         'requests>=2.4',
         'pubnub==4.0.1'
     ],
-    zip_safe=False,
+    zip_safe=True
 )
